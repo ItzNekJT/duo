380d778d72dcfb71e191f4d706d9a94b99eafea5
ItzNek1T <ItzNek1T@mail.com>
2023-02-24 22:46:26 +0600

    Add float.c

---
+++ src/float.c
@@ -0,0 +1,24 @@
+#include <duo.h>
+
+const duoT_type duoG_floattype = {
+    &duoG_typetype,     /* bs_type */
+    -1,                 /* bs_refs */
+
+    &duoG_basetype,     /* tp_base */
+
+    "float",             /* tp_name */
+    sizeof(duoT_float),  /* tp_size */
+
+    NULL,               /* tp_attr */
+    NULL,               /* tp_call */
+};
+
+duoT_float* duoC_float_create(float _data) {
+    duoT_float* ptr = (duoT_float*)
+        duoC_alloc(sizeof(duoT_float));
+    
+    ptr->bs_type = &duoG_floattype;
+    ptr->m_data = _data;
+
+    return ptr;
+}
