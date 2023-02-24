0a23117896aeec24152eeb2aa990cad98a2d7259
ItzNek1T <ItzNek1T@mail.com>
2023-02-24 22:45:17 +0600

    Add duo.h

--- include/duo.h
+++ include/duo.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <duo/base.h>
+#include <duo/type.h>
+
+#include <duo/function.h>
+
+#include <duo/int.h>
+#include <duo/float.h>
+#include <duo/bool.h>
+
+#include <duo/bytearray.h>
+
+#include <duo/table.h>
+
+#include <duo/opcode.h>
