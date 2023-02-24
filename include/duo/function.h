f7199f1b1686b6db8034e3f8322c6ddf30cf32ce
ItzNek1T <ItzNek1T@mail.com>
2023-02-24 22:45:10 +0600

    Add function.h

---
+++ include/duo/function.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <duo/base.h>
+
+typedef struct duoT_function duoT_function;
+
+struct duoT_function {
+    DUO_BASE_HEAD;
+
+    duoT_base* (*m_data)(duoT_base** _args, size_t _nargs);
+};
+
+extern const duoT_type duoG_functiontype;
+
+extern duoT_function* duoC_function_create(duoT_base* (*_data)
+    (duoT_base** _args, size_t _nargs));
+
+extern duoT_base* duoC_function_call(duoT_function* _function,
+    duoT_base** _args, size_t _nargs);
