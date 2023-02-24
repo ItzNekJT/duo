ddddc373564a55896668852f046197303e9ed6cb
ItzNek1T <ItzNek1T@mail.com>
2023-02-24 22:46:18 +0600

    Add int.c

---
+++ src/int.c
@@ -0,0 +1,24 @@
+#include <duo.h>
+
+const duoT_type duoG_inttype = {
+    &duoG_typetype,     /* bs_type */
+    -1,                 /* bs_refs */
+
+    &duoG_basetype,     /* tp_base */
+
+    "int",             /* tp_name */
+    sizeof(duoT_int),  /* tp_size */
+
+    NULL,               /* tp_attr */
+    NULL,               /* tp_call */
+};
+
+duoT_int* duoC_int_create(int _data) {
+    duoT_int* ptr = (duoT_int*)
+        duoC_alloc(sizeof(duoT_int));
+    
+    ptr->bs_type = &duoG_inttype;
+    ptr->m_data = _data;
+
+    return ptr;
+}
