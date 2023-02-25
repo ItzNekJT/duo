6b3dba2ed2938bdeeff859ff7c5474d3c61af039
ItzNek1T <ItzNek1T@mail.com>
2023-02-25 22:45:52 +0600

    Update duo.h

--- include/duo.h
+++ include/duo.h
@@ -3,14 +3,18 @@
 #include <duo/base.h>
 #include <duo/type.h>
 
+#include <duo/none.h>
+
 #include <duo/function.h>
 
 #include <duo/int.h>
 #include <duo/float.h>
 #include <duo/bool.h>
 
-#include <duo/bytearray.h>
+#include <duo/string.h>
 
 #include <duo/table.h>
+
+#include <duo/standart.h>
 
 #include <duo/opcode.h>
