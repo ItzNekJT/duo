78576d284923186f500f6b0d8c3782104f0a34c5
ItzNek1T <ItzNek1T@mail.com>
2023-02-25 22:47:47 +0600

    Update type.c

--- src/type.c
+++ src/type.c
@@ -9,6 +9,21 @@ const duoT_type duoG_typetype = {
     "type",             /* tp_name */
     sizeof(duoT_type),  /* tp_size */
 
-    NULL,               /* tp_attr */
     NULL,               /* tp_call */
 };
+
+bool duoC_type_before(const duoT_type* _type, const duoT_type* _parent) {
+    if (_parent == &duoG_basetype) {
+        return true;
+    }
+
+    while (_type != &duoG_basetype) {
+        if (_type == _parent) {
+            return true;
+        }
+
+        _type = _type->tp_base;
+    }
+
+    return false;
+}
