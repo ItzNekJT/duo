92257dd0024b0553b3ec2790c8c420bd0f2c44c6
ItzNek1T <ItzNek1T@mail.com>
2023-02-25 22:33:40 +0600

    Update base.h

--- include/duo/base.h
+++ include/duo/base.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <assert.h>
+
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -16,15 +18,3 @@ struct duoT_base {
 };
 
 extern const duoT_type duoG_basetype;
-
-extern duoT_base* duoC_base_attr(duoT_base* _self, const char* _attr);
-extern duoT_base* duoC_base_call(duoT_base* _self, duoT_base** _args, size_t _nargs);
-
-extern bool duoC_isinstance(duoT_base* _base, duoT_type* _type);
-
-extern void duoC_incref(duoT_base* _base);
-extern void duoC_decref(duoT_base* _base);
-
-extern void* duoC_alloc(size_t _size);
-extern void* duoC_realloc(void* ptr, size_t size);
-extern void duoC_dealloc(void* _ptr);
