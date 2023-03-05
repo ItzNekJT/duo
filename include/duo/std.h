1008f4892f6bcbf1ddd11dde6a6e11aa08ef8e4c
ItzNek1T <ItzNek1T@mail.com>
2023-03-06 00:05:06 +0600

    Update std.h

--- include/duo/std.h
+++ include/duo/std.h
@@ -3,11 +3,13 @@
 #include <duo/base.h>
 #include <duo/opcode.h>
 
-void* duoC_alloc(size_t _size);
-void* duoC_realloc(void* _ptr, size_t _size);
-void duoC_dealloc(void* _ptr);
+extern void* duoC_alloc(size_t _size);
+extern void* duoC_realloc(void* _ptr, size_t _size);
+extern void duoC_dealloc(void* _ptr);
 
-duoT_base* duoC_call(duoT_base* _base, duoT_base** _args, size_t _nargs);
+extern duoT_base* duoC_call(duoT_base* _base, duoT_base** _args, size_t _nargs);
 
-void duoC_execute(const uint8_t* _code, duoT_table* _scope);
-const uint8_t* duoC_compile(const char* _source);
+extern void duoC_execute(const uint8_t* _code, duoT_table* _scope);
+extern uint8_t* duoC_compile(const char* _source);
+
+extern size_t duoC_hash(const void* _data, size_t _size);
