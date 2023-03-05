526e95afae868d2757adfd21ead09320cba705a9
ItzNek1T <ItzNek1T@mail.com>
2023-03-06 00:05:38 +0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,16 +10,14 @@ duoT_base* duoF_print(duoT_base** _args, size_t _nargs) {
 
 int main(int argc, const char** argv) {
 
-    const uint8_t code[] = {
-        DUO_OPCODE_PUSH_IDENT, 0, 0, 0, 0,
-        DUO_OPCODE_PUSH_STRING, 'H', 'i', ' ', 'D', 'u', 'o', '!', 0,
-        DUO_OPCODE_CALL, 1
-    };
+    const char source[] = "print(\"Hi DUO!\")";
+
+    const uint8_t* code = duoC_compile(source);
 
     duoT_table* scope = duoC_table_create(8);
     duoT_function* print = duoC_function_create(duoF_print);
 
-    duoC_table_insert(scope, print, 0);
+    duoC_table_insert(scope, print, duoC_hash("print", 5));
 
     duoC_execute(code, scope);
     return EXIT_SUCCESS;
