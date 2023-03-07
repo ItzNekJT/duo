570dd2e3c61e834860203811bdedb9cf97ab676c
ItzNek1T <ItzNek1T@mail.com>
2023-03-08 00:03:14 +0600

    Update base.h

--- include/duo/base.h
+++ include/duo/base.h
@@ -1,13 +1,18 @@
 #pragma once
 
+#include <assert.h>
+
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 #define DUO_BASE_HEAD \
     const duoT_type* bs_type; \
     size_t bs_refs
+
+#define duoC_base_type(_base) (((duoT_base*) (_base))->bs_type)
 
 typedef struct duoT_base duoT_base;
 typedef struct duoT_type duoT_type;
